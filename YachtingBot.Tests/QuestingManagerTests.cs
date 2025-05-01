96f9f871b7986f3d182435ac67ccc845c9fe5392
nyxiscoo1 <nyxiscoo1@gmail.com>
2025-05-01 16:29:47 +0300

    Исправлено наличие нечитаемых символов в некоторых вопросах

--- YachtingBot.Tests/QuestingManagerTests.cs
+++ YachtingBot.Tests/QuestingManagerTests.cs
@@ -1,35 +1,34 @@
 ﻿using FluentAssertions;
 
-namespace YachtingBot.Tests
+namespace YachtingBot.Tests;
+
+public class QuestingManagerTests
 {
-    public class QuestingManagerTests
+    [Test]
+    public void Should_handle_sorting_question()
     {
-        [Test]
-        public void Should_handle_sorting_question()
+        const long userId = 123;
+
+        var source = new MultiAnswerQuestion
         {
-            const long userId = 123;
+            Text = "Расставьте возможные ЧП по степени вероятности наступления на борту яхты. Первым - наиболее вероятное событие.",
+            Variants = ["Пожар", "МОВ (человек за бортом)", "Травма членов экипажа", "Затопление", "Критические поломки"],
+            RightAnswers = [2, 1, 0, 3, 4],
+            FixedAnswerOrder = true
+        }.ToQuestions().ToArray();
+        var sut =  new QuestingManager(source);
+        sut.Reset(userId);
+        var q = sut.GetCurrent(userId);
 
-            var source = new MultiAnswerQuestion
-            {
-                Text = "Расставьте возможные ЧП по степени вероятности наступления на борту яхты. Первым - наиболее вероятное событие.",
-                Variants = ["Пожар", "МОВ (человек за бортом)", "Травма членов экипажа", "Затопление", "Критические поломки"],
-                RightAnswers = [2, 1, 0, 3, 4],
-                FixedAnswerOrder = true
-            }.ToQuestions().ToArray();
-            var sut =  new QuestingManager(source);
-            sut.Reset(userId);
-            var q = sut.GetCurrent(userId);
-
-            q.CheckAnswer("Пожар").Should().Be(AnswerResults.Incorrect);
-            q.CheckAnswer("Травма членов экипажа").Should().Be(AnswerResults.NeedMore);
-            q.CheckAnswer("Травма членов экипажа").Should().Be(AnswerResults.Incorrect);
-            q.CheckAnswer("Пожар").Should().Be(AnswerResults.Incorrect);
-            q.CheckAnswer("МОВ (человек за бортом)").Should().Be(AnswerResults.NeedMore);
-            q.CheckAnswer("Травма членов экипажа").Should().Be(AnswerResults.Incorrect);
-            q.CheckAnswer("Пожар").Should().Be(AnswerResults.NeedMore);
-            q.CheckAnswer("Пожар").Should().Be(AnswerResults.Incorrect);
-            q.CheckAnswer("Затопление").Should().Be(AnswerResults.NeedMore);
-            q.CheckAnswer("Критические поломки").Should().Be(AnswerResults.Correct);
-        }
+        q.CheckAnswer("Пожар").Should().Be(AnswerResults.Incorrect);
+        q.CheckAnswer("Травма членов экипажа").Should().Be(AnswerResults.NeedMore);
+        q.CheckAnswer("Травма членов экипажа").Should().Be(AnswerResults.Incorrect);
+        q.CheckAnswer("Пожар").Should().Be(AnswerResults.Incorrect);
+        q.CheckAnswer("МОВ (человек за бортом)").Should().Be(AnswerResults.NeedMore);
+        q.CheckAnswer("Травма членов экипажа").Should().Be(AnswerResults.Incorrect);
+        q.CheckAnswer("Пожар").Should().Be(AnswerResults.NeedMore);
+        q.CheckAnswer("Пожар").Should().Be(AnswerResults.Incorrect);
+        q.CheckAnswer("Затопление").Should().Be(AnswerResults.NeedMore);
+        q.CheckAnswer("Критические поломки").Should().Be(AnswerResults.Correct);
     }
 }
---
+++ YachtingBot.Tests/QuestionSourceTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using FluentAssertions;
+
+namespace YachtingBot.Tests;
+
+public class QuestionSourceTests
+{
+    [Test]
+    public void Answers_should_no_contain_incorrect_symbols()
+    {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        var encoding = Encoding.GetEncoding(1251);
+
+        var hasErrors = false;
+
+        var source = new QuestionSource();
+        foreach (var q in source.LoadQuestions())
+        {
+            foreach (var answer in q.Variants)
+            {
+                var convertedAnswer = encoding.GetString(encoding.GetBytes(answer));
+                var hasQuestions = convertedAnswer.Contains("??");
+                var hasFEFF = answer.Contains('\uFEFF');
+
+                hasErrors |= hasQuestions;
+                hasErrors |= hasFEFF;
+
+                if (hasQuestions || hasFEFF)
+                {
+                    Console.WriteLine(q.Question);
+                    Console.WriteLine("- " + convertedAnswer);
+                }
+            }
+        }
+
+        hasErrors.Should().Be(false);
+    }
+}
